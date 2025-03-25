be757d0965037605d9eb578c075ced575e3efbb3
seyedalihosieni <hoseiniseyeda@gmail.com>
2025-03-25 20:55:14 +0330

    some main file change

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [MainController::class, 'home'])->name('home');
+
+// Authentication Routes
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Admin Routes
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/profile', [AdminController::class, 'profile'])->name('admin.profile');
+    
+    // Product Management Routes
+    Route::resource('products', ProductController::class)->names([
+        'index' => 'admin.products.index',
+        'create' => 'admin.products.create',
+        'store' => 'admin.products.store',
+        'show' => 'admin.products.show',
+        'edit' => 'admin.products.edit',
+        'update' => 'admin.products.update',
+        'destroy' => 'admin.products.destroy',
+    ]);
 });
+
+
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart/products', [CartController::class, 'addProduct'])->name('cart.add');
+Route::put('/cart/products', [CartController::class, 'updateProductQuantity'])->name('cart.update');
+Route::delete('/cart/products', [CartController::class, 'removeProduct'])->name('cart.remove');
+
